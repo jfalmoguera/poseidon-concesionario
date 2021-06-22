a41dd2b9018190c344bcba29fa4d82a242d8d1c7
Jose Almoguera <jalmoguera@bravent.net>
2021-06-22 10:25:54 +0200

    editr desde tarjetas

--- src/app/coches-card-list/coches-card-list.component.ts
+++ src/app/coches-card-list/coches-card-list.component.ts
@@ -10,10 +10,17 @@ export class CochesCardListComponent implements OnInit {
 
   @Input() coches: Coche[] = [];
   @Output() vender = new EventEmitter<string | number | null>();
+  @Output() editar = new EventEmitter<Coche>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  editarClick(coche: Coche): void {
+    this.editar.emit(coche);
+  }
+
+
 
 }
